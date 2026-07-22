13b551b16326ccf775b992677aec9f4ee2b13fe1
LauWai0032 <lau_wai@icloud.com>
2026-07-22 02:19:01 +0000

    ProjectReset

--- web1.0/main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "server.h"
-int main()
-{
-  int lfd = initListenFd(10000);
-  return 0;
-}
--- web1.0/server.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-int initListenFd(unsigned short port);
-int epollRun(int lfd);
-int acceptClient(int lfd,int epfd);
